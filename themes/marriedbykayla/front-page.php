f8b65e538e8f970d5387221b502f707500043abb
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-06 12:50:59 -0800

    hero image made

--- themes/marriedbykayla/front-page.php
+++ themes/marriedbykayla/front-page.php
@@ -9,7 +9,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-      <section>
+      <section class="hero" id="hero">
       </section>
       <section>
       </section>
