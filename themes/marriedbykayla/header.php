b0b60ceb4ee8c89ddde8dce827aeb6f2e236aba6
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-06 14:03:16 -0800

    navigation hamburger styled

--- themes/marriedbykayla/header.php
+++ themes/marriedbykayla/header.php
@@ -26,7 +26,7 @@
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					</div><!-- .site-branding -->
 					<nav id="site-navigation" class="main-navigation" role="navigation">
-						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+						<i class="fa fa-bars menu-toggle" aria-hidden="true" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></i>
 						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 					</nav><!-- #site-navigation -->
 				</div>
