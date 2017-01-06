8a4de107ba8c68a9fcb12228a8b5e33945784cec
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-06 11:55:58 -0800

    color variables made

--- themes/marriedbykayla/front-page.php
+++ themes/marriedbykayla/front-page.php
@@ -9,6 +9,20 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+      <section>
+      </section>
+      <section>
+      </section>
+      <section>
+      </section>
+      <section>
+      </section>
+      <section>
+      </section>
+      <section>
+      </section>
+      <section>
+      </section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
