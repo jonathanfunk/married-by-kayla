d57688d3c0fa08414c92636601943e02694ae6fb
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-06 15:46:18 -0800

    styled about us section

--- themes/marriedbykayla/front-page.php
+++ themes/marriedbykayla/front-page.php
@@ -11,9 +11,17 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
       <section class="hero" id="hero">
       </section>
-      <section>
-      </section>
-      <section>
+      <section class="about" id="about">
+				<h2>About Us</h2>
+				<h3>Who are we?</h3>
+				<div class="about-image">
+					<img src="<?php echo CFS()->get( 'about_us_picture' ); ?>" alt="About Us Picture" />
+				</div>
+				<div class="about-blurb">
+					<div class="about-blurb-wrap">
+						<?php echo CFS()->get( 'about_us_blurb' ); ?>
+					</div>
+				</div>
       </section>
       <section>
       </section>
