5c23e2b8f65da543291154f8932c14bedc5d8ab9
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-12 17:56:18 -0800

    overlay added to navigation

--- themes/marriedbykayla/js/scripts.js
+++ themes/marriedbykayla/js/scripts.js
@@ -4,11 +4,36 @@
  $('.menu-toggle').on('click', function(){
    event.preventDefault();
    $('#primary-menu').animate({height:'toggle'},350);
-  });
+ });
 
-   $('.menu-toggle').on('focusout', function(){
-     event.preventDefault();
-     $('#primary-menu').animate({height:'toggle'},350);
-    });
+ $('.menu-toggle').on('focusout', function(){
+   event.preventDefault();
+   $('#primary-menu').animate({height:'toggle'},350);
+ });
+
+ //This animates scroll navigation
+ var offset = $("header").height();
+ $('a[href*="#"]:not([href="#"])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html, body').animate({
+         scrollTop: target.offset().top - offset
+       }, 1000);
+       return false;
+     }
+   }
+ });
+
+ $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 10) {
+        $("#masthead").addClass("overlay");
+      } else {
+        $("#masthead").removeClass("overlay");
+
+      }
+  });
 
 })( jQuery );
