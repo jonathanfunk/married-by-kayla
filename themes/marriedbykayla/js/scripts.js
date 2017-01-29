1f390456bbbf5eaf06daf1c05e666dde1bad0fbf
Jonathan Funk <jonfunk88@hotmail.com>
2017-01-28 16:43:37 -0800

    took out overlay in nav and implemented drop shadow

--- themes/marriedbykayla/js/scripts.js
+++ themes/marriedbykayla/js/scripts.js
@@ -26,17 +26,6 @@
    }
  });
 
- //This gives the navigation an overlay on scroll.
- $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 10) {
-        $("#masthead").addClass("overlay");
-      } else {
-        $("#masthead").removeClass("overlay");
-
-      }
-  });
-
   $('.faq-text').on('click', function(){
     $(this).toggleClass('move');
   })
